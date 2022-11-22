404ab3a82f854ec4e932a3709919f33b701d0d1d
enriquefnt <enriquefinetti@gmail.com>
2022-11-22 15:50:26 -0300

    enproceso

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -5,13 +5,18 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="../css/styles.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 	<title><?=$title?></title>
 	<script src="../js/autocomplete.js"></script>
 
  <!-- Bootstrap CSS -->
- 				 <link href="../library/bootstrap-5/bootstrap.min.css" rel="stylesheet" />
-        <script src="../library/bootstrap-5/bootstrap.bundle.min.js"></script>
-        <script src="../library/autocomplete.js"></script>
+ 			 <!--  	 <link href="../library/bootstrap-5/bootstrap.min.css" rel="stylesheet" />
+       
+
+       <script src="../library/bootstrap-5/bootstrap.bundle.min.js"></script>
+       <script src="../library/autocomplete.js"></script> -->
 
 
 </head>
