6e0595788b41704a7325e7c816848c10082beb22
enriquefnt <enriquefinetti@gmail.com>
2022-12-15 08:28:14 -0300

    avances ctrl

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -1,30 +1,52 @@
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    
-    <title><?=$title;?></title>
-  </head>
-  <body>
-  <nav>
-    <header>
-      <h1>AUDINUT</h1>
-    </header>
-    <ul>
-      <li><a href="index.php?action=home">Home</a></li>
-      <li><a href="index.php?action=edit">Cargar</a></li>
-      <li><a href="index.php?action=listar">Listar</a></li>
-    </ul>
-  </nav>
+<!DOCTYPE html>
+<html style=" height:100%;">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="../css/styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-  <main>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  <title><?=$title?></title>
+  <script src="../js/autocomplete.js"></script>
+
+</head>
+  <body class="w3-light-grey">
+<header class="p-2 mb-2 bg-primary text-white">
+  <div class="container-fluid">
+<h4>Gestión solicitudes de nutroterápicos</h4>
+<h4>Area Fórmulas</h4>
+  
+
+
+<nav class="navbar navbar-expand-sm navbar-light py-0 small bg-light">
+  <div class="container-fluid"> 
+  
+     <a class="navbar-brand mb-0 " href="index.php?action=home">Home</a>
+      <a class="navbar-brand mb-0 " href="index.php?action=edit">Cargar</a>
+     <a class="navbar-brand mb-0 " href="index.php?action=listar">Listar</a>
+
+  </nav>
+</div>
+</header>
+ <main class="w3-row-padding table-container">  
+  <div class="w3-container" >
+
   <?=$output ?? ''?>
 
-  
+  </div>  
   </main>
+<footer class="p-1 mb-1 bg-primary text-white ">
+<div class="container-fluid">
 
-  <footer>
-  DNyAS
-  </footer>
-  </body>
+<h5 align="center"> DNyAS - Programa: Auditoría Nutroterápicos.</h5>
+
+</div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+
+</body>
 </html>
