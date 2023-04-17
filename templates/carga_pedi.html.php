7dddd4ebda1f7e2c0d8e2664675953be87d39fb9
enriquefnt <enriquefinetti@gmail.com>
2023-04-17 16:08:46 -0300

    ajuste listado

--- templates/carga_pedi.html.php
+++ templates/carga_pedi.html.php
@@ -25,8 +25,8 @@ endif;
     <?=$datosBenef['Nombres'] .' '. $datosBenef['Apellidos']?>
     </legend>
 -->
-	<form onkeydown="return event.key != 'Enter';" class="row g-3"  action="" method="post" 
-	autocomplete="off" >
+	<form onkeydown="return event.key != 'Enter';" class="row g-3"   method="post" 	autocomplete="off" >
+	<form onkeydown="return event.key != 'Enter';" class="row g-3"  action=""  onsubmit="myButton.disabled = true; return true;" method="post" autocomplete="off" >
 		
 	<input type="hidden"name="Pedido[id_datos_pedido]" id="id_datos_pedido" value=<?=$datosBenef['id_datos_pedido'] ?? ''?> 
 >
