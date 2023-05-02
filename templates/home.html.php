f1cb02eba43d89cdd50bbf9479b979344fc82ad8
enriquefnt <enriquefinetti@gmail.com>
2023-05-02 07:47:36 -0300

    edita pedido

--- templates/home.html.php
+++ templates/home.html.php
@@ -1,84 +1,8 @@
 <?php $title="Instructivo" ;?>
 
-<!-- <div class="conteiner container-sm">
-<div> <p class="text-center fs-4 lh-1"><b> Documentación obligatoria </b> </p> </div>
-</div>
-<section class="bg-light pt-5 pb-5 shadow-sm">
-<div class="row">
-
-<div class="col-sm-4">
-<div class="card card-block ">
-  
-  <div class="card-block ">
-  <div class="card-header ">
-  <h4 class="card-title text-center">Primera vez</h4>
-  </div>
-  
-    
-      <ul class="list-group list-group-flush lh-1">
-   <li class="list-group-item "><i class="fa-solid fa-check"></i><b> Prescripción médica</b>, cuya fecha no supere los 30 días, al momento del ingreso del expediente </li>
-   <li class="list-group-item "><i class="fa-solid fa-check"></i><b> Negativa de Anses</b> actualizada.</li>
-   <li class="list-group-item "><i class="fa-solid fa-check"></i><b> Fotocopia de DNI</b> del paciente y familiar a cargo (en caso de ser menor de edad)</li>
-   <li class="list-group-item "><i class="fa-solid fa-check"></i><b> <a href="http://secretariadenutricion.salta.gob.ar/images/varias/Formularios/formulario_A1.pdf" target="_blank"> Formulario A1 </a></b>de pedido de fórmulas nutricionales (lo debe completar el/la nutricionista a cargo del paciente)</li>
-   <li class="list-group-item "><i class="fa-solid fa-check"></i><b> Informe de Asistente social</b> en caso de contar con un profesional en Trabajo Social</li>
-    </ul>
-
-   
-  </div>
-  </div>
-</div>
-          <div class="col-sm-4">
-<div class="card card-block">
-<div class="card-header">
-<h4 class="card-title text-center">Continuación</h4>
-  </div>
-  <div class="card-block">
-    
-    <ul class="list-group list-group-flush lh-1">
-   <li class="list-group-item"><i class="fa-solid fa-check"></i><b> Prescripción médica</b> actualizada.</li>
-   <li class="list-group-item"><i class="fa-solid fa-check"></i><b> Negativa de Anses</b> actualizada</li>
-   <li class="list-group-item"><i class="fa-solid fa-check"></i><b> Fotocopia de DNI </b>del paciente y familiar a cargo (en caso de ser menor de edad)</li>
-   <li class="list-group-item"><i class="fa-solid fa-check"></i><b><a href="http://secretariadenutricion.salta.gob.ar/images/varias/Formularios/formulario_A2.pdf" target="_blank"> Formulario A2 </a></b>de pedido de fórmulas nutricionales (lo debe completar el/la nutricionista a cargo del paciente)</li>
-   <li class="list-group-item"><i class="fa-solid fa-check"></i><b> Informe de Asistente social</b> en caso de contar con un profesional en Trabajo Social</li>
-   <li class="list-group-item"><i class="fa-solid fa-check"></i><b> Pacientes generales</b> cada 2 meses</li>
-   <li class="list-group-item"><i class="fa-solid fa-check"></i><b> Pacientes crónicos </b>cada 6 meses (Certificación médica de cronicidad)</li>
-    </ul>
-     </div>
- 
-</div>
-            
-            
-          </div>
-
-<div class="col-sm-4">
-<div class="card card-block">
-<div  class="card card-block">
-<div class="card-header">
-<h4 class="card-title text-center">Expediente</h4>
-</div>
-  <div class="card-block">
-   
-    <ul class="list-group list-group-flush lh-1">
-    <li class="list-group-item"><i class="fa-solid fa-check"></i><b> Al iniciar</b> el tramite de solicitud de fórmulas se debe generar un EXPEDIENTE con la documentación completa, mediante el sistema SICE (sistema centralizado de expedientes),generando un número para seguimiento</li>
-    <li class="list-group-item"><i class="fa-solid fa-check"></i><b> Enviar </b>cada expediente generado con la CARATULA correspondiente con los datos del paciente completos</li>
-<ul>
-<li class="list-group-item"><i class="fa-solid fa-check text-yellow"></i><b> Apellidos y Nombres </b>completos en mayúscula</li>
-<li class="list-group-item"><i class="fa-solid fa-check"></i><b> DNI</b> sin los puntos</li>
-</ul>
-
-    
-  </ul>
-   
-  </div>
-  </div>
-</div>
-</div>
-  
-</div>
-</section> -->
 
 <section class="bg-light pt-5 pb-5 shadow-sm">
-  <div class="container">
+ <div class="conteiner container-sm">
     <div class="row pt-5">
       <div class="col-12">
         <h3 class="text-uppercase border-bottom mb-1 text-center"><b> Documentación obligatoria </b> </p></h3>
@@ -88,7 +12,7 @@
       <!--ADD CLASSES HERE d-flex align-items-stretch-->
       <div class="col-lg-4 mb-3 d-flex align-items-stretch">
         <div class="card">
-        <h5 class="card-header text-center">Inicio</h5>
+        <h5 class="card-header text-center bg-primary">Inicio</h5>
           <div class="card-body d-flex flex-column">
             
             <ul class="list-group list-group-flush lh-1">
@@ -100,10 +24,9 @@
     </ul>
            
           </div>
-          <div class="card-footer text-muted">
-          <p ><i class="fa-solid fa-check"></i><b> Pacientes generales</b> cada 2 meses</p>
-          <p><i class="fa-solid fa-check"></i><b> Pacientes crónicos </b>cada 6 meses (Certificación médica de cronicidad)</p>
-  </div>
+            <div class="card-footer text-muted">
+            <p ><i class="fa-solid fa-exclamation fa-beat"></i>Para<b> Pacientes generales</b> renovación cada 2 meses y para<b> Pacientes crónicos </b>cada 6 meses adjuntando certificación médica de cronicidad</p>
+            </div>
         </div>
       </div>
       <!--ADD CLASSES HERE d-flex align-items-stretch-->
@@ -122,8 +45,7 @@
     </ul>
           </div>
           <div class="card-footer text-muted">
-          <p ><i class="fa-solid fa-check"></i><b> Pacientes generales</b> cada 2 meses</p>
-          <p><i class="fa-solid fa-check"></i><b> Pacientes crónicos </b>cada 6 meses (Certificación médica de cronicidad)</p>
+            <p ><i class="fa-solid fa-exclamation fa-beat"></i> Para<b> Pacientes generales</b> renovación cada 2 meses y para<b> Pacientes crónicos </b>cada 6 meses adjuntando certificación médica de cronicidad</p>
   </div>
         </div>
       </div>
@@ -137,11 +59,10 @@
     <li class="list-group-item"><i class="fa-solid fa-check"></i><b> Al iniciar</b> el tramite de solicitud de fórmulas se debe generar un EXPEDIENTE con la documentación completa, mediante el sistema SICE (sistema centralizado de expedientes),generando un número para seguimiento</li>
     <li class="list-group-item"><i class="fa-solid fa-check"></i><b> Enviar </b>cada expediente generado con la CARATULA correspondiente con los datos del paciente completos</li>
 <ul>
-          </div>
-          <div class="card-footer text-muted">
-          <p ><i class="fa-solid fa-exclamation fa-beat"></i><b> Pacientes generales</b> cada 2 meses</p>
-          <p><b> Pacientes crónicos </b>cada 6 meses (Certificación médica de cronicidad)</p>
-  </div>
+            </div>
+            <div class="card-footer text-muted">
+            <p ><i class="fa-solid fa-exclamation fa-beat"></i>Para<b> Pacientes generales</b> renovación cada 2 meses y para<b> Pacientes crónicos </b>cada 6 meses adjuntando certificación médica de cronicidad</p>
+        </div> 
         </div>
       </div>
     </div>
