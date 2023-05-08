b1604559109d732678e077e73fdcd3209a8fcdff
enriquefnt <enriquefinetti@gmail.com>
2023-05-08 09:00:10 -0300

    anda cargar no editar

--- ClassPart/Controllers/Pedidos.php
+++ ClassPart/Controllers/Pedidos.php
@@ -7,7 +7,7 @@ class Pedidos {
 private $pediTable;
 private $userTable;
 private $benefTable;
-
+private $authentication;
 
 public function __construct(\ClassGrl\DataTables $pediTable,
 							\ClassGrl\DataTables $benefTable,
@@ -20,8 +20,7 @@ public function __construct(\ClassGrl\DataTables $pediTable,
 		$this->authentication = $authentication;		
     }
 
-
-
+/// Metodo si es GET //////  
 
 
 
@@ -52,27 +51,72 @@ if (isset($_GET['id'])) {
 
 
 
+
+/// Metodo si es con post //////   
+
+
 public function pedidoSubmit() {
 
-		$usuario = $this->authentication->getUser();
+	$usuario = $this->authentication->getUser();
 
-		$pedido=$_POST['Pedido'];
-		$pedido['usuari_id']= $usuario['id_usuario'] ?? '00';
-		$pedido['fecha_ped']= new \DateTime();
+	$pedido=$_POST['Pedido'];
+	$pedido['usuari_id']= $usuario['id_usuario'] ?? '00';
+	$pedido['fecha_ped']= new \DateTime();
 
-	$errors = [];
-	if ($_SESSION['tipo'] > 3) {
-	$errors[] = 'Ud no está habilitado para cargar pedidos';
-	}
+$errors = [];
+if ($_SESSION['tipo'] > 3) {
+$errors[] = 'Ud no está habilitado para cargar pedidos';
+}
 if  (empty($errors)) {
 
 
-	$this->pediTable->save($pedido);
+$this->pediTable->save($pedido);
 }
-	
-	header('Location: /user/success');
+
+header('Location: /user/success');
 }
 
 
+public function listar(){
 
-}
+	$result = $this->pediTable->find('id_datos_benef',$_GET['id']);
+	$datosBenef = $this->benefTable->findById($_GET['id']);
+
+		$pedidos = [];
+		foreach ($result as $pedido) {
+			
+			$pedidos[] = [
+				'id_datos_pedido' => $pedido['id_datos_pedido'],
+				'fecha_ped' =>  date('d/m/Y',strtotime($pedido['fecha_ped'])),
+				'nutro_ter' => $pedido['nutro_ter'],
+				'env_pormes' => $pedido['env_pormes'],
+				'estado' => $pedido['estado'],
+				'id_datos_benef'=> $pedido['id_datos_benef']
+			];
+
+		}
+		$result = $this->pediTable->findAll();
+		
+		$title = 'Carga Pedidos';
+
+		$totalPedi = $this->pediTable->total();
+
+
+		return ['template' => 'listaped.html.php',
+				'title' => $title,
+				'variables' => ['totalPedi' => $totalPedi,
+				'pedidos' => $pedidos,
+				'datosBenef' => $datosBenef  ?? ' ']
+			];
+	}
+
+
+
+
+
+public function success() {
+return ['template' => 'registersuccess.html.php',
+'title' => 'Registro OK'];
+		}
+
+	}
