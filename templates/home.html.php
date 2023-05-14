250c7158c309e3ba3e53efdc859c4733eef0d1b2
Enrique Finetti <enriquefinetti@gmail.com>
2023-05-13 21:01:40 -0300

    anda al fin editar

--- templates/home.html.php
+++ templates/home.html.php
@@ -113,55 +113,11 @@
             Direccion de Nutrición y ALimentación Saludable, organismo perteneciente al Ministerio de Salud Pública de Salta.
           </p>
         </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <!-- <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4"> -->
-          <!-- Links -->
-          <!-- <h6 class="text-uppercase fw-bold mb-4">
-            Products
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">Angular</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">React</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Vue</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Laravel</a>
-          </p>
-        </div> -->
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <!-- <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4"> -->
-          <!-- Links -->
-          <!-- <h6 class="text-uppercase fw-bold mb-4">
-            Useful links
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">Pricing</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Settings</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Orders</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Help</a>
-          </p>
-        </div> -->
-        <!-- Grid column -->
-
-        <!-- Grid column -->
+        
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- Links -->
           <h6 class="text-uppercase fw-bold mb-4">Contacto</h6>
-          <p><i class="fas fa-home me-3 text-secondary"></i> Belgrano 1340 -1º piso - Salta Capital - CP 4400</p>
+          <p><i class="fas fa-home me-3 text-secondary"></i> Belgrano 1349 -1º piso - Salta Capital - CP 4400</p>
           <p><i class="fas fa-envelope me-3 text-secondary"></i>areaformulasnutricionales@gmail.com </p>
           <p><i class="fas fa-phone me-3 text-secondary"></i> 0387-4370128/127</p>
           <p><i class="fas fa-print me-3 text-secondary"></i> 0387-4320695</p>
